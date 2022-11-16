659f67035c6c6fe5abd3b3c20865fc9083600f20
Brandon Zamorano <BrandonZamorano@users.noreply.github.com>
2022-11-16 10:12:29 -0600

    finish exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,10 +1,15 @@
 import Part from './Part'
-const Content = ({parts}) => {
+const Content = ({ parts }) => {
+    const totalExercises = parts
+        .map(part => part.exercises)
+        .reduce((prev, curr) => prev + curr);
+
     return (
         <div>
             {parts.map(part => (
                 <Part key={part.id} part={part} />
             ))}
+            <p><b>total of {totalExercises} exercises</b></p>
         </div>
     )
 }
